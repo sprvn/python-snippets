b129942bdd4f570f790559945f186772fe778f56
Kevin Wentling <kevin.wentling@sinch.com>
2021-10-21 16:27:45 +0200

    Added unittest

---
+++ test/test_dummy.py
@@ -0,0 +1,19 @@
+"""Unit tests docstring."""
+
+import unittest
+
+class TestConfigParser(unittest.TestCase):
+    """Functions to test functionality of."""
+
+    def setUp(self):
+        """Initial setup to run before every test."""
+        pass
+
+    def test_something(self):
+        self.assertEqual(1,1)
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ unittest.md
@@ -0,0 +1,6 @@
+# Running unit tests
+Stand outside of the `test` directory and run:
+
+```
+% python -m unittest discover
+```
